4abd1ed006b1175c9277724ed14a71efe867cbc1
Bartek Bak <bartek.bak@protonmail.com>
2025-02-27 17:57:45 +0000

    feat: add heading

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,7 @@
 <script>
+  import Header from "./Header.svelte";
 </script>
 
-<main class="max-w-[21cm] mx-auto">
-  <h1 class="text-4xl font-bold">Hello world!</h1>
-  <p class="text-xl">
-    This is a project to showcase my skills and experience as a software
-    developer.
-  </p>
+<main class="max-w-[21cm] mx-auto pt-16">
+  <Header />
 </main>
--- src/Header.svelte
+++ src/Header.svelte
@@ -1,9 +1,35 @@
 <script>
-  import picture from "./picture.jpg";
+  import picture from "/picture.jpg";
+  import { MapPin, Github, Mail } from "lucide-svelte";
 </script>
 
-<header>
-  <div>
-    <p>Bartosz Bak</p>
+<header class="flex justify-between gap-16">
+  <div class="flex flex-col gap-4">
+    <p class="text-3xl font-bold">Bartosz Bak</p>
+    <p class="font-geist-mono text-lg">
+      Student and an aspiring full-stack software engineer who likes to craft
+      nice things.
+    </p>
+    <p class="flex gap-2 items-center font-geist-mono text-md text-gray-500">
+      <MapPin size={16} /> York, United Kingdom, UTC
+    </p>
+
+    <div class="flex gap-5 items-center">
+      <a
+        href="https://github.com/nocdn"
+        class="flex gap-2 items-center font-geist-mono text-md text-gray-700 transition-colors hover:text-blue-800"
+      >
+        <Github size={16} /> github.com/nocdn
+      </a>
+      <a
+        href="mailto:contact@bartoszbak.org"
+        class="flex gap-2 items-center font-geist-mono text-md text-gray-700 transition-colors hover:text-blue-800"
+      >
+        <Mail size={16} /> contact@bartoszbak.org
+      </a>
+    </div>
+  </div>
+  <div class="flex-shrink-0 w-48 h-48 rounded-4xl">
+    <img src={picture} alt="Bartosz Bak" class="rounded-4xl" />
   </div>
 </header>
