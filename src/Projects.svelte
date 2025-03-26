221e4d3c3094a9b7bffb4563fbdd3469854a7d26
Bartek Bak <bartek.bak@protonmail.com>
2025-03-26 23:47:27 +0000

    feat: added project cards, now to fill them in with my projects

--- src/Projects.svelte
+++ src/Projects.svelte
@@ -1,3 +1,15 @@
+<script>
+  import Project from "./lib/Project.svelte";
+</script>
+
 <main class="flex flex-col gap-4">
   <p class="text-2xl font-bold">Projects</p>
+  <projects>
+    <Project
+      title="Transcriptions"
+      description="Full-stack, self-hostable and containerised app for transcribing audio and video files with whisper, gemini, etc"
+      demo="https://whisper.bartoszbak.org"
+      repo="https://github.com/nocdn/transcriptions"
+    />
+  </projects>
 </main>
---
+++ src/lib/Project.svelte
@@ -0,0 +1,60 @@
+<script>
+  let { title, description, demo, repo, upToDate = false } = $props();
+  import { BadgeCheck } from "lucide-svelte";
+  let showPopover = $state(false);
+</script>
+
+<project
+  class="flex flex-col gap-2 max-w-xs border border-gray-200 rounded-xl px-3.5 py-3"
+>
+  <div class="flex justify-between items-center">
+    <p
+      class="font-geist text-[17px] font-medium inline-flex items-center gap-2"
+    >
+      {title}
+      <span
+        role="tooltip"
+        class="relative"
+        onfocus={() => {
+          showPopover = true;
+        }}
+        onmouseover={() => {
+          showPopover = true;
+        }}
+        onmouseleave={() => {
+          showPopover = false;
+        }}
+      >
+        <BadgeCheck class="text-blue-700" size={16} strokeWidth={2.65} />
+        {#if showPopover}
+          <!-- svelte-ignore node_invalid_placement_ssr -->
+          <div
+            class="w-52 h-fit absolute bottom-6 left-1/2 -translate-x-1/2 bg-white rounded-xl border border-gray-200 shadow-xl z-10 p-3 font-geist-mono text-sm animate-preview-popover-up"
+          >
+            <p class="text-center">
+              This project is currently actively <span
+                class="text-blue-800 font-semibold">maintained.</span
+              >
+            </p>
+          </div>
+        {/if}
+      </span>
+    </p>
+    <div class="flex gap-3 items-center">
+      <a
+        href={repo}
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-sm font-geist-mono text-gray-500 hover:text-gray-700"
+        >Repo</a
+      ><a
+        href={demo}
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-sm font-geist-mono text-blue-700 hover:text-blue-600"
+        >Demo</a
+      >
+    </div>
+  </div>
+  <p class="font-geist-mono text-[15px]">{description}</p>
+</project>
